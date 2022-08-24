b21b663f7981aa8abb83d33e2dae8376000c1cc3
sharifidavood <sharifidavood743@gmail.com>
2022-08-24 01:10:59 -0700

    addBaseofSlider

--- src/Components/ContentfirstPage/MainSlider/MainSlider.jsx
+++ src/Components/ContentfirstPage/MainSlider/MainSlider.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { CityData } from './SliderSource/Source/data';
 
 // Import Swiper styles
 import 'swiper/css';
 import './mainSlider.scss'
 
 const MainSlider = () => {
+  console.log(CityData)
   return (
     <Swiper className='main-frame-slider'
-    spaceBetween={50} Autoplay={true}
-    slidesPerView={3} onSlideChange={() => console.log('slide change')}
+    spaceBetween={50} autoplay pagination
+    slidesPerView={1} onSlideChange={() => console.log('slide change')}
     onSwiper={(swiper) => console.log(swiper)}>
-    <SwiperSlide>esfahan</SwiperSlide>
-    <SwiperSlide>shairaz</SwiperSlide>
-    <SwiperSlide>lorestan</SwiperSlide>
-    <SwiperSlide>Kordestan</SwiperSlide>
-    ...
+   {
+    CityData.map((val,key)=>{
+      console.log(val,key)
+      return(<>
+      <SwiperSlide className='sliderFram' key={key}>
+        <p className='sliderTitle'>{val.name}</p>
+        <img className='slider-imgs' src={val.imgSrc} key={val.id} />
+      </SwiperSlide>
+      
+      </>)
+
+    })
+   }
   </Swiper>
   )
 }
---
+++ src/Components/ContentfirstPage/MainSlider/SliderSource/Source/data.js
@@ -0,0 +1,38 @@
+import es from '../Source/esfahan.jpg';
+import ghazvin from '../Source/ghazvin.jpg';
+import lor from '../Source/lorestan.jpg';
+import shir from '../Source/shairaz.jpg';
+import kord from '../Source/kordestan-2.jpg'
+
+
+export const CityData=[
+    {
+        id:"1",
+        name:"Esfahan",
+        imgSrc:es
+
+    },
+    {
+        id:2,
+        name:"ghazvin",
+        imgSrc:ghazvin
+
+    },
+    {
+        id:3,
+        name:"lorestan",
+        imgSrc:lor
+
+    },
+    {
+        id:4,
+        name:"shiraz",
+        imgSrc:shir
+
+    }
+    ,{
+        id:5,
+        name:"kordestan",
+        imgSrc:kord
+    }
+] 
